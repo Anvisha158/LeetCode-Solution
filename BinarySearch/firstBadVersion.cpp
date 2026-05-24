42084cbeca977bc85daa18834192b9bfb591f0bb
Anvisha158 <anvisha_2401cs41@iitp.ac.in>
2026-05-24 15:28:24 +0530

    Added 4 lettcode solution

---
+++ BinarySearch/firstBadVersion.cpp
@@ -0,0 +1,20 @@
+# include<iostream>
+# include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int firstBadVersion(int n){
+        int low=1;
+        int high=n;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(isBadVersion(mid)){
+                high=mid;
+            }
+            else low=mid+1;
+        }
+        return low;
+    }
+};
+// Time complexity:-O(log n)
+//space complexity:- O(1)
---
+++ BinarySearch/searchInsertPosition.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int searchInsert(vector<int>& nums,int target){
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            if (nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+
+            }
+            else high=mid-1;
+        }
+        return low;
+    }
+};
