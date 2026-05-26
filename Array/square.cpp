9d7f461ef0652deb281f0243747adf76e0e44393
Anvisha158 <anvisha_2401cs41@iitp.ac.in>
2026-05-26 12:35:25 +0530

    Contain with most water solution

--- Array/square.cpp
+++ Array/square.cpp
@@ -13,4 +13,32 @@ class Solution{
     sort(nums.begin(),nums.end());
     return nums;
     }
+
 };
+// time complexity:-O(nlogn)
+// space complexity:-O(1)
+
+// use two pointer approach
+class Solution{
+    public:
+    vector<int> sortedSquares(vector<int>& nums){
+        int n=nums.size();
+        vector<int> result(n);
+        int left=0;
+        int right=n-1;
+        int i=n-1;// start filling the reasult array from the end
+        while(left<=right){
+            if(nums[left]*nums[left]>nums[right]*nums[right]){
+                result[i]=nums[left]*nums[left];
+                left++;
+            }
+            else{
+                result[i]=nums[right]*nums[right];
+                right--;
+            }
+        }
+        return result;
+    }
+};
+// time complexity:-O(n)
+// space complexity:-O(n)
