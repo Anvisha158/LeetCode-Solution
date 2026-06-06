a4d9da101a8012acf1ea55f3f291f1118aae8932
Anvisha158 <anvisha_2401cs41@iitp.ac.in>
2026-06-06 12:44:33 +0530

    LinkedListCycleSolution

---
+++ LinkedList/cycle.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+struct ListNode{
+    int data;
+    ListNode* next;
+};
+class Solution{
+    public:
+    bool hascycle(ListNode* head){
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};
