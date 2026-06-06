7edbb18989cddd56bfa052bcd922176278d835d1
Anvisha158 <anvisha_2401cs41@iitp.ac.in>
2026-06-06 12:06:28 +0530

    MiddleOfLinkedListSolution

---
+++ LinkedList/middle.cpp
@@ -0,0 +1,19 @@
+# include<iostream>
+# include<bits/stdc++.h>
+using namespace std;
+struct ListNode{
+    int data;
+    ListNode* next;
+};
+class Solution{
+    public:
+    ListNode* middleNode(ListNode* head){
+        ListNode* slow=head;
+        ListNode*fast=head;
+        while(fast!=NULL&& fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+};
