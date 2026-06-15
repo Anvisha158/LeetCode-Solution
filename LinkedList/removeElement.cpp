ebf3e658f022afad91fe5c30a2ed801b22eb209c
Anvisha158 <anvisha_2401cs41@iitp.ac.in>
2026-06-15 12:37:55 +0530

    remove the LinkedList Element Solution

---
+++ LinkedList/removeElement.cpp
@@ -0,0 +1,28 @@
+# include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode(int x){
+        val=x;
+        next=NULL;
+    }
+};
+class Solution{
+    public:
+    ListNode* removeElements(ListNode* head,int val){
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        ListNode* curr=dummy;
+        while(curr->next!=NULL){
+            if(curr->next->val==val){
+                curr->next=curr->next->next;
+            }
+            else{
+                curr=curr->next;
+            }
+        }
+        return dummy->next;
+    }
+};
